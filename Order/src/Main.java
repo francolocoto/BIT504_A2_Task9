bfb3c410b7a9053d59980c876370e305c3a85cca
francolocoto <franklinfalculan@gmail.com>
2026-04-29 18:31:38 +1200

    Initial upload of files

---
+++ Order/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("What is your name? ");
+				String input = scanner.next();
+				System.out.println("Hello, " + input + "!");
+
+	}
+}
