661724135fc85b5e477a4dbc7c919558a9411816
francolocoto <franklinfalculan@gmail.com>
2026-04-29 18:33:43 +1200

    Added Menu to show user

--- Order/src/Main.java
+++ Order/src/Main.java
@@ -7,6 +7,11 @@ public class Main {
 				System.out.println("What is your name? ");
 				String input = scanner.next();
 				System.out.println("Hello, " + input + "!");
+				
+				System.out.println("It is nice to meet you!\n");
+				System.out.println("What do you like to order:\n" +
+				"1. Chocolate\n2. Vanilla\n3. Strawberry.\n");
+
 
 	}
 }
